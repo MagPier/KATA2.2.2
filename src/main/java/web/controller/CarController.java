b059c5e80b8cfadb1ce3ed89e455ed206e171444
Magpier <garson.ao@gmail.com>
2023-11-24 03:27:01 +0300

    Задача 2.2.2 версия 3 Убрал логику из контроллера, добавил автоподстановку CarDao

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,10 +5,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.Dao.CarDao;
-import web.Dao.CarDaoImpl;
-import web.model.Car;
 
-import java.util.List;
 
 @Controller
 public class CarController {
